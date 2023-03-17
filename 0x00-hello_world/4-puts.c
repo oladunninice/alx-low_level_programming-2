d873ef2ea31faea6b5d630d4460c6fed3e4db06e
oladunni igbokwe <ayokanice1gmail.com>
2023-03-16 19:00:12 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that uses put function to print aline
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
