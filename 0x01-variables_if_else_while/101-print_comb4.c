c2380c84c75a75b507223a9340b9d961aed4f96e
oladunni igbokwe <ayokanice1gmail.com>
2023-03-19 08:12:46 -0700

    do more of what you do

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (j = 2 ; j < 10 ; j++)
+		{
+			for (k = 3 ; k < 10 ; k++)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+				putchar(',');
+				putchar(' ');
+			}
+
+
+
+		}
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+my frist c programm
