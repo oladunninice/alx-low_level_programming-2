3a0c10bda84df1307962d984bee06092dc51852a
oladunni igbokwe <ayokanice1gmail.com>
2023-03-20 03:58:23 -0700

    UNIX

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+        fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
+
+	return (1)
+}
