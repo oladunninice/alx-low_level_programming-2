ab5600310a855e7b66155059d8d26a7319618e81
oladunni igbokwe <ayokanice1gmail.com>
2023-03-23 13:12:22 -0700

    numbers speak

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers: numbers 0-9 in new lines
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	char (c);
+
+	for (c = '0' ; c <= '9' ; c++)
+	{
+		_putchar(print_numbers);
+	}
+	_putchar('\n');
+}
