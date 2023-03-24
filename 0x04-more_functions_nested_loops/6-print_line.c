665b900d145163300d3702882f3b3c391db96701
oladunni igbokwe <ayokanice1gmail.com>
2023-03-23 18:47:12 -0700

    universal language

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *  print_line - printing a line
+ *
+ * Return: void
+ */
+
+void print_line(int n)
+{
+        char a
+
+
+        for (i = 1; i <= 10; i++)
+        {
+                for (j = 0; j <= 14; j++)
+                {
+                        if (j >= 14)
+                                _putchar('1');
+                        _putchar(j % 10 + '0');
+                }
+                _putchar('\n');
+        }
+}
