87311b0f4898f8d99e93e87742ec38d0b626aea4
oladunni igbokwe <ayokanice1gmail.com>
2023-03-23 20:47:43 -0700

    straight line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -5,20 +5,13 @@
  *
  * Return: void
  */
-
 void print_line(int n)
 {
-        char a
+	int x;
 
-
-        for (i = 1; i <= 10; i++)
-        {
-                for (j = 0; j <= 14; j++)
-                {
-                        if (j >= 14)
-                                _putchar('1');
-                        _putchar(j % 10 + '0');
-                }
-                _putchar('\n');
-        }
+	for (x = 0; x < n; x++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
 }
