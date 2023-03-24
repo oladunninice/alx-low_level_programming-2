54ff728eba210288033a469d27bb466aec334dc7
oladunni igbokwe <ayokanice1gmail.com>
2023-03-24 14:26:13 -0700

    prime

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -22,7 +22,7 @@ void print_triangle(int size)
 			{
 				_putchar(' ');
 			}
-			for (hashes = 1; hashes <= row; hashes--)
+			for (hashes = 1; hashes <= row; hashes++)
 			{
 				_putchar('#');
 			}
