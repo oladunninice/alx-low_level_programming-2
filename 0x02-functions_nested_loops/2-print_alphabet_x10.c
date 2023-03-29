20cc84ca95f6dab70747dc526a58af1a9cce1eaf
oladunni igbokwe <ayokanice1gmail.com>
2023-03-29 12:32:36 -0700

    x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,13 +8,14 @@
  * Return: always 0:
  */
 
-void print_alphabet_x10(void)
+void print_alphabet_x10(void);
+
 {
 
 int i;
 char j;
 i = 0;
-while (i >= 10)
+while (i < 10)
 {
 for (j = 'a' ; j <= 'z'; j++)
 {
