b09a01ed002601018a0262aad99696c1a0ddd677
oladunni igbokwe <ayokanice1gmail.com>
2023-04-03 07:31:42 -0700

    2. strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
+ *
+ * Return: change array with new value for n bytes
+ */
+
+char *_strchr(char *s, char c)
+{
+int i = 0;
+
+for (; s[i] >= '\0'; i++)
+{
+if (s[i] == c)
+}
+return (0);
+
+}
