dccb21330f0f6d497e0d89f979f92cd372f77b98
oladunni igbokwe <ayokanice1gmail.com>
2023-04-03 08:50:05 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strstr - entry point
+ * @haystack: input
+ * @needle: input
+ * Return: always 0
+ */
+char *_strstr(char *haystack, char *needle)
+{
+for (; *haysrack != '\0'; haystack++)
+{
+char *l = haystack;
+char *p = needle;
+
+while (*l == *p && *p != '\0')
+{
+l++;
+p++;
+}
+if (*p == '\0')
+return (haystack);
+}
+return (0);
+}
