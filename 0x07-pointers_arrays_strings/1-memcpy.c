d9dca3232d03c6e2e6e5528ddf78a0765e99eeda
oladunni igbokwe <ayokanice1gmail.com>
2023-04-03 07:04:29 -0700

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _memcpy - function that copies memory area
+ * @dest: memory stored
+ * @src: memory copied
+ * @n: number of bytes
+ *
+ * Return: copied memory with bytes changed
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+int r = 0;
+int i = n;
+
+for (; r < i; r++)
+{
+dest[r] = b;
+n--;
+}
+return (dest);
+
+}
