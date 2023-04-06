de2181fb897f919479c5c5900ba30791639a265c
oladunni igbokwe <ayokanice1gmail.com>
2023-04-06 06:30:50 -0700

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * factorial - returns the factorial of a number
+ * @n: number to be returned to the factorial from
+ * Return: factorial of n
+ */
+int factorial of n
+{
+if (n < 0 )
+return (-1);
+if (n == 0)
+return (0);
+return (n * factorial(n - 1 ));
+}
---
+++ 0x08-recursion/3_factorial.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *@
+ * Return: Always 1
+ */
+int factorial(int n)
+{
