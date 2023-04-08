a385e08de3c2080b352260f7a9faef7561197eb4
oladunni igbokwe <ayokanice1gmail.com>
2023-04-08 03:37:25 -0700

    name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print its name
+ * @argc: count
+ * @argv: string of array
+ * Return: always 0
+ */
+
+int main(int argc__attribute__((unused)), char *argv[])
+{
+printf("%d\n", argv);
+return (0);
+}
