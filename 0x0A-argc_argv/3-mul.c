751aaacfd110bd0f193c354339e11a53bb9335cd
oladunni igbokwe <ayokanice1gmail.com>
2023-04-09 12:22:05 -0700

    ican

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _atoi - convent a string to an integer
+ * @s: strings to be convented
+ *
+ * Return: the int convented to strings
+ */
+
+int _atoi(char *s)
+{
+	int 
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,5 +2,8 @@
 
 int _putchar(char c);
 int _atoi(char *s);
+int main(int argc__attribute__((unused)), char *argv[]);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
 
 #endif
