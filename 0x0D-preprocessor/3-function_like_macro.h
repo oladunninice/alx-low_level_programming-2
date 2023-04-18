a34fc76c3aa433414062f6755b081e8a1785b426
oladunni igbokwe <ayokanice1gmail.com>
2023-04-17 19:29:37 -0700

    file2

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,15 +1,6 @@
-#include <stdio.h>
-#include "3-function_like_macro.h"
-#include "3-function_like_macro.h"
+#ifndef FUNCTION_ LIKE_MACRO_H
+#define FUNCTION_ LIKE_MACRO_H
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
+#define ABS(x) ((x) < (0) ? -(x) : (x))
 
-#define ABS (x) ((x) * (x))
-int main(void)
-{
-
-}
+#endif
